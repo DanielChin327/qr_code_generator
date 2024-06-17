b399a9efdb44b3f3c60edc9612c282346491f2cd
Daniel Chin <DanChin327@gmail.com>
2024-06-17 14:25:46 -0600

    added JS variables

---
+++ script.js
@@ -0,0 +1,8 @@
+let imgBox = document.querySelector("#imgBox");
+let qrImage = document.querySelector("#qrImage");
+let qrText = document.querySelector("#qrText");
+
+function generateQR(){
+  qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+
+}
