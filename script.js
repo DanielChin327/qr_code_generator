83f77eab4f1af84280accfe369108257aaf81193
Daniel Chin <DanChin327@gmail.com>
2024-06-17 14:46:14 -0600

    added if to the function so that generates only with length above 1

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let qrImage = document.querySelector("#qrImage");
 let qrText = document.querySelector("#qrText");
 
 function generateQR(){
+  if(qrText.value.length > 0) {
   qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
   imgBox.classList.add("show-img");
+  }
 }
